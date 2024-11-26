1ce89f9c50cb5306cc4bdd651b88a4cb7b56f73c
Jahid Hasan <jahid.hasan.shanto@|gmail.com>
2024-11-26 17:23:27 +0600

    products sale added

--- src/components/home/FeaturedProducts.jsx
+++ src/components/home/FeaturedProducts.jsx
@@ -9,7 +9,7 @@ export default function FeaturedProducts() {
         <div className="container mx-auto relative">
             <SectionHeader title="Featured Products" styleClass="text-gray-900" />
             <div className="my-12">
-                <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-5 mx-auto container justify-items-center">
+                <div className="flex flex-row gap-5 justify-center items-center flex-wrap mx-auto container justify-items-center">
                     {featuredProduct.map((product) => (
                         <Product key={product.id} product={product}/>
                     ))}
--- src/components/partials/Product.jsx
+++ src/components/partials/Product.jsx
@@ -17,7 +17,12 @@ const Product = ({ product }) => {
                 <div className="p-4 flex justify-between">
                     <div className="">
                         <h3 className="text-sm text-gray-800">{product.name}</h3>
+                        {product.discount?
+                        ( <p className="font-medium text-gray-900">$<span>{product.discount.price_after_discount.toFixed(2)}</span>  $<span className="line-through font-normal text-gray-500">{product.price.toFixed(2)}</span></p>
+                        ):(
                         <p className="font-medium text-gray-900">$ <span>{product.price.toFixed(2)}</span></p>
+                        )}
+                        
                         <div className="flex flex-row">
                             {Array.from({ length: product.rating }).map((_, index) => (
                                 <Star key={index} />
@@ -39,10 +44,14 @@ const Product = ({ product }) => {
                         <ShoppingBag w={15} h={15} />
                     </button>
                 </div>
-                <div className="text-sm text-white flex flex-row py-1 px-2 rounded gap-2 absolute top-4 left-4 bg-red ">
-                    <p>Sale</p>
-                    <p className="font-medium">80%</p>
-                </div>
+                {product.discount?(
+
+                    <div className="text-sm text-white flex flex-row py-1 px-2 rounded gap-2 absolute top-4 left-4 bg-red ">
+                        <p>Sale</p>
+                        <p className="font-medium">{product.discount.percentage}%</p>
+                    </div>
+                ):(<></>)}
+                
             </div>
         </>
     );
